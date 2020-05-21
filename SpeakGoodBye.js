e9461003983bacba190faa499819f69d0a151366
mdeepan <deepan.manohar@gmail.com>
2020-05-21 10:04:17 +0800

    Passed the window object in the function call

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -4,7 +4,7 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 
-(function () {
+(function (window) {
 	// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 	// the "speak" method and which you will expose to the global context
 	// See Lecture 52, part 1
@@ -27,5 +27,5 @@
 	// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 	// 'byeSpeaker' on the global scope as well.
 	window.byeSpeaker = byeSpeaker;
-})();
+})(window);
 
